9e57d02dd2d9cab25826da8aa97ca97f7f4dfff8
Evan Noeth <128528172+EvanNoeth@users.noreply.github.com>
2024-12-08 23:47:58 -0600

    completed evaluator? I think?

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -6,3 +6,42 @@
 //for evaluation use recursion to solve for leftNode [OPERATOR] rightNode
 //note that leaves (nodes with no children) will be numbers and internal nodes will be operators
 
+#include <iostream>
+#include <string>
+#include "expressionTree.cpp"
+
+class Evaluator {
+public:
+    double evaluate(ExpressionNode* node) {
+        if (!node) {
+            throw "Null node encountered in expression tree.";
+        }
+
+        NumberNode* numberNode = dynamic_cast<NumberNode*>(node);
+        if (numberNode) {
+            return numberNode->number;
+        }
+
+        OperatorNode* operatorNode = dynamic_cast<OperatorNode*>(node);
+        if (operatorNode) {
+            double leftValue = evaluate(operatorNode->leftChild);
+            double rightValue = evaluate(operatorNode->rightChild);
+
+            switch (operatorNode->operation) {
+                case '+': return leftValue + rightValue;
+                case '-': return leftValue - rightValue;
+                case '*': return leftValue * rightValue;
+                case '/':
+                    if (rightValue == 0) {
+                        throw "Division by zero.";
+                    }
+                    return leftValue / rightValue;
+                default:
+                    throw "Invalid operator encountered.";
+            }
+        }
+
+        throw "Unknown node type encountered.";
+    }
+};
+
